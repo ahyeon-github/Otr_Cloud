b2ba661013daa1ae88fdeef9861bb10de752f7cc
AhyeonLim <ahyeon31@sookmyung.ac.kr>
2022-08-03 15:07:31 +0900

    이미저장 구현

--- myvenv/bin/activate.fish
+++ myvenv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/ahyeonlim/YUSEO-Server/myvenv"
+set -gx VIRTUAL_ENV "/Users/ahyeonlim/Otr-Server/myvenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- yuseo/yuseoapp/models.py
+++ yuseo/yuseoapp/models.py
@@ -7,11 +7,13 @@ class Yuseo(models.Model):
     title = models.CharField(max_length=100)
     date = models.DateTimeField(auto_now_add=True)
     body = models.TextField()
+    image = models.ImageField(upload_to='%Y/%m/%d', null=True)
+  
     
     def __Str__(self):
         return self.title
     
     def summary(self):
         return self.body [:30]
-    
-    
+
+
