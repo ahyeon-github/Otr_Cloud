0239048dda16cf185f2181587479f708cd3be030
Kim Jisu <xoxo1795@sookmyung.ac.kr>
2022-08-15 02:14:20 +0900

    QuestionRepresnetationSerializer 주석처리

--- yuseo/QnA/serializers.py
+++ yuseo/QnA/serializers.py
@@ -6,12 +6,21 @@ class QuestionSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = QuestionSeven
-        fields = ('title', 'subtitle')
+        fields = ('id', 'title', 'subtitle')
         
+
 class AnswerSerializer(serializers.ModelSerializer):
     class Meta:
         model = AnswerSeven
-        fields = ('body',)
+        fields = ('q_id', 'body',)
 
-        
+    def to_representation(self, instance):
+        self.fields['q_id']= QuestionRepresentationSerializer(read_only=True)
+        return super(AnswerSerializer, self).to_representation(instance)
+
     
+class QuestionRepresentationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= QuestionSeven
+        fields=("id", "title", "subtitle", "body")
+        
